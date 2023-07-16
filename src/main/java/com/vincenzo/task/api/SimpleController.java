eaf3b97cebccecf63b3cc6e4b763572c1e2cbebe
vincenzo <vincenzo.911gts@gmail.com>
2023-07-16 22:53:20 +0900

    fix simple

--- src/main/java/com/vincenzo/task/api/SimpleController.java
+++ src/main/java/com/vincenzo/task/api/SimpleController.java
@@ -1,9 +1,11 @@
 package com.vincenzo.task.api;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/v1/test")
+@RestController
+@RequestMapping("/v1/test")
 public class SimpleController {
 
     @GetMapping("/simple")
